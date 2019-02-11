3cb9d355102562e91374994d13a396ae6ade96b0
RSingh39 <rishabh_singh39@yahoo.com>
2019-02-10 21:01:48 -0700

    Changed the features

--- Classifier.py
+++ Classifier.py
@@ -2,10 +2,14 @@
 # classification of minivans and sports cars
 from sklearn import tree
 
-# include features of the cars by engine hp x seats
-features = [[440, 2], [500, 2], [190, 9], [150, 8]]
+# include features of the image by resolution x mega pixels
+# using the following resolutions in sequence
+# 1280 x 720, 1920 x 1080, 3840 x 2160 and 7680 x 4320
+# the mega pixels are multiplying the following resolutions above in the list
+# to be able to find it's mega pixels
+features = [[720, 0.9], [1080, 2.0], [3840, 8.2], [7680, 33.1]]
 # labeled the training data
-# labels = ["Sports", "Sports", "MiniVan", "MiniVan"]
+# labels = ["High Def", "High Def", "Ultra High Def", "Ultra High Def"]
 labels = [0, 0, 1, 1]
 
 # create a classifier which is a decision tree
@@ -16,11 +20,11 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 
 # place unknown data
-# predicting its a minivan with 160HP and 7 seats
-print(clf.predict([[160, 7]]))
+# predicting its a High Def with 1280 x 720 resolution with 0.9 mega pixels or higher
+print(clf.predict([[780, 1.3]]))
 
-# predicting its a sports car with 600HP and 2 seats
-print(clf.predict([[600, 2]]))
+# predicting its a Ultra High Def with 3840 x 2160 resolution with 8.2 mega pixels or higher
+print(clf.predict([[4820, 10]]))
 
 
 
